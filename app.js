7aecf23c9dffbf21a8e573ad90cfe23090922cff
Kevin Lee <kleekev@sas.upenn.edu>
2023-06-16 20:30:09 -0400

    Added hover feature

--- app.js
+++ app.js
@@ -1,27 +1,34 @@
-function removeChildren(parent) {
-    while (parent.firstChild) {
-        parent.removeChild(parent.firstChild);
-        console.log(parent.firstChild);
-    }
+// Function to add hover feature on the grid
+function addHoverFunction() {
+    const grid = document.querySelectorAll('#square');
+    [...grid].forEach(function(item) {
+        item.addEventListener('mouseover', () => {
+            item.style.backgroundColor = 'black';
+            console.log(item)
+        });
+    });
 }
-
+// Funtion to add grid squares to a div container based on the grid size
 function addGridSquares(gridSize, container) {
     for (let i = 0; i < gridSize * gridSize; i++) {
         const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('id', 'square');
         squareDiv.setAttribute('style', `min-height:${Math.floor(512/gridSize)}px; min-width:${Math.floor(512/gridSize)}px; background-color:white; display:inline-block; margin:0; padding:0`);
         container.appendChild(squareDiv);
     }
 }
 
+// Initialization
 const middle = document.querySelector('.middle');
 const container = document.createElement('div');
 container.setAttribute('style', 'height: 512px; width: 512px; display:flex; flex-wrap:wrap;');
 container.setAttribute('class', 'container');
 middle.appendChild(container);
-
 let gridSize = 16;
 addGridSquares(gridSize, container);
+addHoverFunction();
 
+// Button Functionality
 const button = document.querySelector('.size');
 button.addEventListener('click', () => {
     gridSize = Number(prompt("Enter Grid Size"));
@@ -32,9 +39,8 @@ button.addEventListener('click', () => {
     const oldContainer = document.querySelector('.container');
     middle.removeChild(oldContainer);
     middle.appendChild(newContainer);
-    console.log(gridSize)
+    addHoverFunction();
 })
-
 
 
 
