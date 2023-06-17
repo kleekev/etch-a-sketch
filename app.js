46985dd6434aa1f44ba429c8ffb84047819fcbfd
Kevin Lee <kleekev@sas.upenn.edu>
2023-06-16 20:00:43 -0400

    Added functionality to button

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
+function removeChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild);
+        console.log(parent.firstChild);
+    }
+}
+
+function addGridSquares(gridSize, container) {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        const squareDiv = document.createElement('div');
+        squareDiv.setAttribute('style', `min-height:${Math.floor(512/gridSize)}px; min-width:${Math.floor(512/gridSize)}px; background-color:white; display:inline-block; margin:0; padding:0`);
+        container.appendChild(squareDiv);
+    }
+}
+
 const middle = document.querySelector('.middle');
 const container = document.createElement('div');
 container.setAttribute('style', 'height: 512px; width: 512px; display:flex; flex-wrap:wrap;');
+container.setAttribute('class', 'container');
 middle.appendChild(container);
 
+let gridSize = 16;
+addGridSquares(gridSize, container);
 
-for (let i = 0; i < 256; i++) {
-    const squareDiv = document.createElement('div');
-    squareDiv.setAttribute('style', `min-height:${Math.ceil(512/16)}px; min-width:${Math.ceil(512/16)}px; background-color:white; display:inline-block; margin:0; padding:0`);
-    container.appendChild(squareDiv);
-}
+const button = document.querySelector('.size');
+button.addEventListener('click', () => {
+    gridSize = Number(prompt("Enter Grid Size"));
+    const newContainer = document.createElement('div');
+    newContainer.setAttribute('style', 'height: 512px; width: 512px; display:flex; flex-wrap:wrap;');
+    newContainer.setAttribute('class', 'container');
+    addGridSquares(gridSize, newContainer);
+    const oldContainer = document.querySelector('.container');
+    middle.removeChild(oldContainer);
+    middle.appendChild(newContainer);
+    console.log(gridSize)
+})
+
+
+
+
+
+
